62be8e90351d6e8134dfccd466ddf667c83b25e0
Wojciech Przybyła <contact@wpww.pl>
2025-02-13 21:47:04 +0100

    💫 fixed toast animation

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,6 @@
 @props(['status'])
 
-<x-tile class="alert shaded accent {{ $status }}">
+<x-tile class="alert shaded accent {{ $status }} interactive">
     <div class="flex right middle">
         @svg("mdi-".($status == "success" ? "check-circle" : "alert-circle"))
 

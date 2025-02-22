53c0d76f589ec70ac32d9d5648133d733467a45a
Wojciech Przybyła <contact@wpww.pl>
2025-02-22 23:39:38 +0100

    💄 changed search bar style

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,8 +1,8 @@
 <search>
-    <form action="" class="grid middle bordered rounded padded accent tertiary">
+    <form action="" class="grid middle rounded padded">
         <input type="text" placeholder="Jakiego szkolenia szukasz?">
 
-        <button type="submit" class="rounded accent background tertiary">
+        <button type="submit" class="rounded accent background secondary">
             @svg("mdi-magnify")
         </button>
     </form>

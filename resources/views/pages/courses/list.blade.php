8a55497d71a9728382fe755744e58461f45366a5
Wojciech Przybyła <contact@wpww.pl>
2025-02-26 21:20:33 +0100

    course-listing 🚧 creating layout

--- resources/views/pages/courses/list.blade.php
+++ resources/views/pages/courses/list.blade.php
@@ -4,8 +4,18 @@
 
 @section("content")
 
+<x-full-width class="bulletpoints">
+    <h1>Wyszukiwarka aktualnych <b>kursów i szkoleń</b> dla logopedów dostępnych w Polsce!</h1>
+
+    <div class="grid col3">
+        @foreach (json_decode(App\Models\Setting::get("course_bulletpoints")) ?? [] as $bp)
+
+        @endforeach
+    </div>
+</x-full-width>
+
 <x-full-width>
-    <x-side-content-container>
+    <x-side-content-container flipped>
         <x-h :icon="\App\Models\Course::META['icon']">{{ \App\Models\Course::META['label'] }}</x-h>
 
         @forelse ($courses as $course)

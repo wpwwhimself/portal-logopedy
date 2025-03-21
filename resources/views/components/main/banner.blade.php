d5794e0433e8d0336cb275ad3aa89951f9be4d1c
Wojciech Przybyła <contact@wpww.pl>
2025-03-21 17:08:59 +0100

    💄 updated background of main banner to be adaptive to dark mode

--- resources/views/components/main/banner.blade.php
+++ resources/views/components/main/banner.blade.php
@@ -7,7 +7,6 @@
 @if ($action) <a href="{{ $action }}"> @endif
 
 <div class="banner full-width flex right center middle">
-    <div class="background" style="background-image: url('{{ $src }}');"></div>
     <img src="{{ $src }}" alt="{{ $alt }}">
 </div>
 
